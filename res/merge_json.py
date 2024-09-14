668ba1de52f376231f918ed2baaa06ed4b135694
AvgBlue <4646dudu@gmail.com>
2024-09-14 15:44:08 +0300

    Refactor merge_json.py to update DragonData with corresponding labels from TextLabel.json

--- res/merge_json.py
+++ res/merge_json.py
@@ -1,47 +1,33 @@
+# res\\TextLabel.json
+# res\\DragonData.json
+
 import json
 
-# Load data from both files
-with open('CharaData_filtered.json') as chara_file, open('abilitiesTextDescriptions.json') as abilities_file:
-    chara_data = json.load(chara_file)
-    abilities_data = json.load(abilities_file)
+# Load the JSON files
+with open('res\\DragonData.json', 'r') as dragon_file, open('res\\TextLabel.json', 'r') as text_label_file:
+    dragon_data = json.load(dragon_file)
+    text_label_data = json.load(text_label_file)
 
-# Function to update abilities with "BelongsTo"
-def add_belongsto(chara_data, abilities_data):
-    for chara_id, chara in chara_data.items():
-        # Get the character's name or both name and second name
-        if chara["_Name"] == chara["_SecondName"]:
-            chara_name = chara["_Name"]
-        else:
-            chara_name = f'{chara["_Name"]}, {chara["_SecondName"]}'
-        
-        # List of abilities attributes to check
-        ability_fields = [
-            "_Abilities11", "_Abilities12", "_Abilities13", "_Abilities14",
-            "_Abilities21", "_Abilities22", "_Abilities23", "_Abilities24",
-            "_Abilities31", "_Abilities32", "_Abilities33", "_Abilities34",
-            "_ExAbility2Data1", "_ExAbility2Data2", "_ExAbility2Data3", 
-            "_ExAbility2Data4", "_ExAbility2Data5"
-        ]
-        
-        # For each ability field, check if it's valid and not zero
-        for field in ability_fields:
-            ability_id = chara.get(field, 0)
-            if ability_id != 0 and str(ability_id) in abilities_data:
-                ability_entry = abilities_data[str(ability_id)]
-                
-                # Add "BelongsTo" attribute or update it
-                if "BelongsTo" not in ability_entry:
-                    ability_entry["BelongsTo"] = set()
-                ability_entry["BelongsTo"].add(chara_name)
+# Helper function to get the label from TextLabel based on ID
+def get_text_label(label_id, text_label_data):
+    return text_label_data.get(label_id, {}).get('_Text', '')
+
+# Iterate over the entries in DragonData and replace _Name and _SecondName with corresponding labels
+for dragon_entry in dragon_data['dict']['entriesValue']['Array']:
+    # Get the _Name and _SecondName values (e.g., "DRAGON_NAME_20050415")
+    name_key = dragon_entry.get('_Name', '')
+    second_name_key = dragon_entry.get('_SecondName', '')
+
+    # Replace the _Name with the corresponding label text from TextLabel.json
+    if name_key:
+        dragon_entry['_Name'] = get_text_label(name_key, text_label_data)
     
-    # Convert sets back to lists for JSON serialization
-    for ability in abilities_data.values():
-        if "BelongsTo" in ability:
-            ability["BelongsTo"] = list(ability["BelongsTo"])
+    # Replace the _SecondName with the corresponding label text from TextLabel.json
+    if second_name_key:
+        dragon_entry['_SecondName'] = get_text_label(second_name_key, text_label_data)
 
-# Add the "BelongsTo" attribute
-add_belongsto(chara_data, abilities_data)
+# Save the modified data back to a new JSON file
+with open('ModifiedDragonData.json', 'w') as output_file:
+    json.dump(dragon_data, output_file, indent=4)
 
-# Save the updated abilities file
-with open('updated_abilitiesTextDescriptions.json', 'w') as updated_abilities_file:
-    json.dump(abilities_data, updated_abilities_file, indent=4)
+print("DragonData has been successfully updated and saved to 'ModifiedDragonData.json'")
